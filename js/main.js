5e11521fe534245791ce01de90b217ee4d91bd60
Евраз <kostousik@mail.ru>
2019-09-24 10:49:38 +0300

    Вносит правки в соответствии с замечаниями

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var MAX_GUEST = 10;
-var MIN_PRICE = 10000;
-var MAX_PRICE = 1000000;
-var MAX_ROOMS = 5;
+var MAX_ADULTS = 10;
+var MIN_COST = 10000;
+var MAX_COST = 1000000;
+var MAX_SPACE = 5;
 var MIN_Y = 130;
 var MAX_Y = 630;
 var NUMBER_OF_ITEMS = 8;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
-var TITLES = ['Park Hotel Tokyo',
+var NAME_OF_HOTELS = ['Park Hotel Tokyo',
   'Tokyo Prince Hotel',
   'Tokyo Ariake Bay Hotel',
   'Act Hotel Shibuya',
   'Tokyu Stay Aoyama Premier',
   'Belken Hotel Kanda'];
 
-var TYPES = ['palace', 'flat', 'house', 'bungalow'];
+var ACCOMODATIONS = ['palace', 'flat', 'house', 'bungalow'];
 
 var TIMES = ['12:00', '13:00', '14:00'];
 
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var ACTIVITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-var DESCRIPTIONS = ['Потрясающий вид на город, отель удобно расположен в Токио, всего в нескольких шагах от станции метро Shiodome.',
+var INFO = ['Потрясающий вид на город, отель удобно расположен в Токио, всего в нескольких шагах от станции метро Shiodome.',
   'Отель Tokyo Prince расположен всего в 3 минутах ходьбы от телевизионной башни Токио.',
   'Капсульный отель в 4 минутах ходьбы от железнодорожного вокзала Синономэ на линии Ринкай.',
   'Отель Act Shibuya удобно расположен в районе Мегуро Вард, в 600 м от храма Камимегуро-Хикава, в 800 м от парка Сугекари.',
   'Отель Tokyu Stay Aoyama Premier находится в 2 минутах ходьбы от станции метро Gaienmae.',
   'Отель Belken Kanda расположен в Токио, в 2,1 км от Императорского дворца и в 2,7 км от национального стадиона сумо «Рёгоку Кокугикан.'];
 
-var PHOTOS = [
+var IMAGES = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
@@ -39,7 +39,7 @@ var map = document.querySelector('.map');
 var mapPinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var pinListElement = document.querySelector('.map__pins');
 
-var hideMap = function () {
+var mapActivate = function () {
   map.classList.remove('map--faded');
 };
 
@@ -49,19 +49,15 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var getRandomInt = function (max) {
-  return Math.floor(Math.random() * Math.floor(max));
-};
-
-var getRandomArr = function (arr) {
-  var randomArr = [];
-  var arrayLength = getRandomInt(getRandomIntInclusive(1, 10));
-  for (var i = 0; i < arrayLength; i++) {
-    randomArr.push(arr[i]);
+function shuffleArray(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
   }
-
-  return randomArr;
-};
+  return arr;
+}
 
 var getRandomArrElement = function (array) {
   var random = array[Math.floor(Math.random() * array.length)];
@@ -71,22 +67,22 @@ var getRandomArrElement = function (array) {
 var createData = function (number) {
   var rents = [];
 
-  for (var i = 1; i < number; i++) {
+  for (var i = 1; i <= number; i++) {
     rents.push({
       author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+        avatar: 'img/avatars/user0' + i + '.png'
       },
       offer: {
-        title: getRandomArrElement(TITLES),
-        price: getRandomIntInclusive(MIN_PRICE, MAX_PRICE),
-        type: getRandomArrElement(TYPES),
-        rooms: getRandomIntInclusive(1, MAX_ROOMS),
-        guests: getRandomIntInclusive(1, MAX_GUEST),
+        title: getRandomArrElement(NAME_OF_HOTELS),
+        price: getRandomIntInclusive(MIN_COST, MAX_COST),
+        type: getRandomArrElement(ACCOMODATIONS),
+        rooms: getRandomIntInclusive(1, MAX_SPACE),
+        guests: getRandomIntInclusive(1, MAX_ADULTS),
         checkin: getRandomArrElement(TIMES),
         checkout: getRandomArrElement(TIMES),
-        features: getRandomArr(FEATURES),
-        description: getRandomArrElement(DESCRIPTIONS),
-        photos: getRandomArr(PHOTOS)
+        features: shuffleArray(ACTIVITIES),
+        description: getRandomArrElement(INFO),
+        photos: shuffleArray(IMAGES)
       },
 
       location: {
@@ -120,7 +116,7 @@ var createPins = function (rents) {
 };
 
 var newArray = function () {
-  hideMap();
+  mapActivate();
   var advArray = createData(NUMBER_OF_ITEMS);
   createPins(advArray);
 };
