1ebe4f202f6df05adbe6e4c7f144ba39f428c315
Евраз <kostousik@mail.ru>
2019-09-28 23:36:22 +0300

    Выполняет задание Личный проект: больше деталей

--- js/main.js
+++ js/main.js
@@ -1,29 +1,59 @@
 'use strict';
 
+var NUMBER_OF_ITEMS = 8;
 var MAX_ADULTS = 10;
 var MIN_COST = 10000;
 var MAX_COST = 1000000;
 var MAX_SPACE = 5;
 var MIN_Y = 130;
 var MAX_Y = 630;
-var NUMBER_OF_ITEMS = 8;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
-var NAME_OF_HOTELS = ['Park Hotel Tokyo',
+var typeOffer = {
+  'flat': 'Квартира',
+  'bungalo': 'Бунгало',
+  'house': 'Дом',
+  'palace': 'Дворец'
+};
+
+var NAME_OF_HOTELS = [
+  'Park Hotel Tokyo',
   'Tokyo Prince Hotel',
   'Tokyo Ariake Bay Hotel',
   'Act Hotel Shibuya',
   'Tokyu Stay Aoyama Premier',
   'Belken Hotel Kanda'];
 
-var ACCOMODATIONS = ['palace', 'flat', 'house', 'bungalow'];
+var ADDRESS = [
+  'Tokyo, Minato-ku Higashi Shimbashi 1-7-1',
+  'Tokyo, Chiyoda-ku Yurakucho 1-2-1',
+  'Tokyo, Koto-ku Ariake 3-7-11',
+  'Tokyo, Chuo-ku Nihonbashi Kakigara-cho 2-1-1',
+  'Tokyo, Minato-ku Akasaka 3-12-5',
+  'Tokyo, Shinjuku-ku Yotsuya 2-1'];
 
-var TIMES = ['12:00', '13:00', '14:00'];
+var ACCOMODATIONS = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow'];
 
-var ACTIVITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'];
 
-var INFO = ['Потрясающий вид на город, отель удобно расположен в Токио, всего в нескольких шагах от станции метро Shiodome.',
+var ACTIVITIES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'];
+
+var INFO = [
+  'Потрясающий вид на город, отель удобно расположен в Токио, всего в нескольких шагах от станции метро Shiodome.',
   'Отель расположен всего в 3 минутах ходьбы от телевизионной башни Токио.',
   'Капсульный отель в 4 минутах ходьбы от железнодорожного вокзала Синономэ на линии Ринкай.',
   'Отель удобно расположен в районе Мегуро Вард, в 600 м от храма Камимегуро-Хикава, в 800 м от парка Сугекари.',
@@ -38,6 +68,8 @@ var IMAGES = [
 var map = document.querySelector('.map');
 var mapPinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var pinListElement = document.querySelector('.map__pins');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var mapFiltersContainer = document.querySelector('.map__filters-container');
 
 var activateMap = function () {
   map.classList.remove('map--faded');
@@ -69,6 +101,7 @@ var createData = function (number) {
       },
       offer: {
         title: getRandomArrElement(NAME_OF_HOTELS),
+        address: getRandomArrElement(ADDRESS),
         price: getRandomIntInclusive(MIN_COST, MAX_COST),
         type: getRandomArrElement(ACCOMODATIONS),
         rooms: getRandomIntInclusive(1, MAX_SPACE),
@@ -110,10 +143,50 @@ var createPins = function (rents) {
   pinListElement.appendChild(fragment);
 };
 
-var createNewArray = function () {
+function renderCards(advertisment) {
+  var mapMark = cardTemplate.cloneNode(true);
+  var photoFragment = document.createDocumentFragment();
+  var photoElement = mapMark.querySelector('.popup__photo');
+  var photoPart = mapMark.querySelector('.popup__photos');
+  var featurePart = mapMark.querySelector('.popup__features');
+  mapMark.querySelector('.popup__title').textContent = advertisment.offer.title;
+  mapMark.querySelector('.popup__text--address').textContent = advertisment.offer.address;
+  mapMark.querySelector('.popup__text--price ').textContent = advertisment.offer.price + '₽/ночь';
+  mapMark.querySelector('.popup__type').textContent = typeOffer[advertisment.offer.type];
+  mapMark.querySelector('.popup__text--capacity').textContent = advertisment.offer.rooms + ' комнаты для ' + advertisment.offer.guests + ' гостей';
+  mapMark.querySelector('.popup__text--time').textContent = 'Заезд после ' + advertisment.offer.checkin + ', выезд до ' + advertisment.offer.checkout;
+  mapMark.querySelector('.popup__description').textContent = advertisment.offer.description;
+  mapMark.querySelector('.popup__avatar').src = advertisment.author.avatar;
+
+  featurePart.innerHTML = '';
+  for (var i = 0; i < advertisment.offer.features.length; i++) {
+    var feature = document.createElement('li');
+    feature.classList.add('popup__feature');
+    feature.classList.add('popup__feature--' + advertisment.offer.features[i]);
+    featurePart.append(feature);
+  }
+
+  photoPart.innerHTML = '';
+  for (var j = 0; j < advertisment.offer.photos.length; j++) {
+    var photo = photoElement.cloneNode(true);
+    photo.src = advertisment.offer.photos[j];
+    photoFragment.appendChild(photo);
+  }
+  photoPart.appendChild(photoFragment);
+
+  return mapMark;
+}
+
+var addCard = function (advItem) {
+  var advertisment = renderCards(advItem);
+  map.insertBefore(advertisment, mapFiltersContainer);
+};
+
+var init = function () {
   activateMap();
   var advArray = createData(NUMBER_OF_ITEMS);
   createPins(advArray);
+  addCard(advArray[0]);
 };
 
-createNewArray();
+init();
